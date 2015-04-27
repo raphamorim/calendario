9ef667114a8c21f4a92f1551ef6e2b2f588db8f8
Raphael Amorim <rapha850@gmail.com>
2015-04-26 23:06:05 -0300

    make first things

--- src/calendario.js
+++ src/calendario.js
@@ -3,14 +3,38 @@
 var lib = require('../lib');
 
 var Calendario = function(calendar) {
-    this.calendar = calendar;
-    console.log(calendar);
+    this.calendar = calendar.toUpperCase();
+
+    var path = '../calendars/' + this.calendar + '/' + 
+        this.calendar + '-national.json';
+    this.calendarData = require(path);
+};
+
+Calendario.prototype.getEvents = function(dateFormat) {
+    var self = this,
+        data = self.calendarData;
+
+    var calendarEvents = data['VCALENDAR'][0]['VEVENT'],
+        events = [];
+
+    calendarEvents.forEach(function(ev) {
+        if(dateFormat === ev['DTSTART;VALUE=DATE']) 
+            events.push(ev);
+    });
+
+    return events;
 };
 
 Calendario.prototype.isWorkday = function(date) {
     var self = this;
-    
-    console.log(date, self.calendar)
+
+    var dateStart = date.getFullYear() + ''
+        + ('0' + (date.getMonth() + 1)).slice(-2) + ''
+        + ('0' + date.getDate()).slice(-2);
+
+    if (self.getEvents(dateStart).length > 0)
+        return false;
+
     return true;
 }
 
