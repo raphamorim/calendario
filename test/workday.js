e64951e76a3e1e764aa493892da0fc62af9ee2f0
Raphael Amorim <rapha850@gmail.com>
2015-05-02 16:45:34 -0300

    update tests

---
+++ test/workday.js
@@ -0,0 +1,63 @@
+var assert = require('assert'),
+	calendario = require('../index.js');
+
+describe('Workday', function() {
+	context("US • United States National", function() {
+		context('- using Thanksgiving Day', function() {
+			it('should get false', function(done) {
+				calendario.use('US'),
+				thanksgiving = calendario.isWorkday(new Date('2015-11-26 10:00'));
+
+				assert.equal(thanksgiving, false);
+				done();
+			});
+		});
+		context('- using Christmas', function() {
+			it('should get false', function(done) {
+				calendario.use('US')
+				christmas = calendario.isWorkday(new Date('2015-12-25 23:00'));
+
+				assert.equal(christmas, false);
+				done();
+			});
+		});
+		context('- using a Workday', function() {
+			it('should get true', function(done) {
+				calendario.use('US')
+				workday = calendario.isWorkday(new Date('2015-10-05 05:00'));
+
+				assert.equal(workday, true);
+				done();
+			});
+		});
+	});
+	context('BR • Brazil National', function() {
+		context('- using Independence Day', function() {
+			it('should get false', function(done) {
+				calendario.use('BR')
+				independence = calendario.isWorkday(new Date('2015-09-07 10:00'));
+
+				assert.equal(independence, false);
+				done();
+			});
+		});
+		context('- using Christmas', function() {
+			it('should get false', function(done) {
+				calendario.use('BR')
+				christmas = calendario.isWorkday(new Date('2015-12-25 12:00'));
+
+				assert.equal(christmas, false);
+				done();
+			});
+		});
+		context('- using a Workday date', function() {
+			it('should get true', function(done) {
+				calendario.use('BR')
+				workday = calendario.isWorkday(new Date('2015-10-05 15:00'));
+
+				assert.equal(workday, true);
+				done();
+			});
+		});
+	});
+});
