de04f97f82c201bb5a05ffcf4e827ef256bd6b1f
Raphael Amorim <rapha850@gmail.com>
2015-05-03 22:54:18 -0300

    clean index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 module.exports = require('./src/calendario.js');
-
-var calendario = require('./src/calendario.js');
-calendario.use('BR');
-calendario.use('US');
-
-console.log(calendario.sourceList());
-
-console.log(calendario.isWorkday(new Date('2015-12-01')));
